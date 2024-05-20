b42c31c14da8dec44a1a8a7dd7bf20f3ea8db4e1
Adenbroughton <aden.broughton@barclays.com>
2024-05-20 15:36:01 +0100

    Part 3

--- src/app.js
+++ src/app.js
@@ -3,19 +3,38 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 //TODO: Create your GET Request Route Below: 
 
-app.get("/restaurants", async (require, response) => {
+app.get("/restaurants", async (request, response) => {
     const restaurant = await Restaurant.findAll();
     response.json(restaurant);
 });
 
-app.get("/restaurants/:id", async (require, response) => {
+app.get("/restaurants/:id", async (request, response) => {
     const number = require.params.id;
     const restaurant = await Restaurant.findByPk(number);
     response.json(restaurant);
 }
 )
 
+app.post("/restaurants", async (request, response) => {
+    const restaurant = await Restaurant.create(request.body);
+    response.json(restaurant);
+});
+
+app.put("/restaurants/:id", async (request, response) => {
+    const updatedRest = await Restaurant.update(request.body, {where: {id: request.params.id}
+    });
+    response.json(updatedRest);
+})
+
+app.delete("/restaurants/:id", async (request, response) => {
+    const deletedRest = await Restaurant.destroy({where: {id: request.params.id}
+    });
+    response.json(updatedRest);
+})
 
 module.exports = app;
